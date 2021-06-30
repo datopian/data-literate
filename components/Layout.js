bda24e42014d1520c764e830fe03472cb58ad7bc
Rufus Pollock <rufus.pollock@datopian.com>
2021-06-30 17:17:40 +0200

    [theme][m]: add a navbar based off tailwindui.

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,18 @@
-export default function Layout({ children }) {
+import Link from 'next/link'
+import Head from 'next/head'
+
+import Nav from '../components/Nav'
+
+export default function Layout({ children, title = 'Data Literate' }) {
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Nav />
       <div className="prose mx-auto p-6">
         {children}
       </div>
