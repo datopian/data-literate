4d37728057c68a9f2fb196e5dd6b3723691de384
Rufus Pollock <rufus.pollock@datopian.com>
2021-07-04 13:16:40 +0200

    [theme][s]: add footer with datopian logo and tweak title setting.

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,11 @@ import Head from 'next/head'
 
 import Nav from '../components/Nav'
 
-export default function Layout({ children, title = 'Data Literate' }) {
+export default function Layout({ children, title = 'Home' }) {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>Data Literate - {title}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -16,6 +16,17 @@ export default function Layout({ children, title = 'Data Literate' }) {
       <div className="prose mx-auto p-6">
         {children}
       </div>
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://datopian.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built by{' '}
+          <img src="/datopian-logo.png" alt="Datopian Logo" className="h-6 ml-2" />
+        </a>
+      </footer>
     </>
   )
 }
