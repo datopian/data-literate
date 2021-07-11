eba78e8f2749b7da8d96c48b92526b17b081ac92
Rufus Pollock <rufus.pollock@datopian.com>
2021-07-11 22:32:52 +0200

    [refactor][m]: factor out the Data Literate document into separate component and dedicated markdown parser so it can be reused in different pages (topics page coming soon).

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,60 +1,16 @@
 import fs from 'fs'
-import matter from 'gray-matter'
-import toc from 'remark-toc'
-import slug from 'remark-slug'
-import gfm from 'remark-gfm'
-import footnotes from 'remark-footnotes'
-
-import { MDXRemote } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import Link from 'next/link'
 import path from 'path'
 
-import CustomLink from '../components/CustomLink'
-import { Vega, VegaLite } from 'react-vega'
+import parse from '../lib/markdown.js'
 
-import Layout from '../components/Layout'
+import DataLiterate from '../components/DataLiterate'
+
 import { postFilePaths, POSTS_PATH } from '../utils/mdxUtils'
 
-// Custom components/renderers to pass to MDX.
-// Since the MDX files aren't loaded by webpack, they have no knowledge of how
-// to handle import statements. Instead, you must include components in scope
-// here.
-const components = {
-  a: CustomLink,
-  // It also works with dynamically-imported components, which is especially
-  // useful for conditionally loading components for certain routes.
-  // See the notes in README.md for more details.
-  TestComponent: dynamic(() => import('../components/TestComponent')),
-  Table: dynamic(() => import('../components/Table')),
-  Excel: dynamic(() => import('../components/Excel')),
-  // TODO: try and make these dynamic ...
-  Vega: Vega,
-  VegaLite: VegaLite,
-  LineChart: dynamic(() => import('../components/LineChart')),
-  Head,
-}
 
 export default function PostPage({ source, frontMatter }) {
   return (
-    <Layout>
-      <header>
-        <div className="mb-6">
-          <h1>{frontMatter.title}</h1>
-          {frontMatter.author && (
-            <div className="-mt-6"><p className="opacity-60 pl-1">{frontMatter.author}</p></div>
-          )}
-          {frontMatter.description && (
-            <p className="description">{frontMatter.description}</p>
-          )}
-        </div>
-      </header>
-      <main>
-        <MDXRemote {...source} components={components} />
-      </main>
-    </Layout>
+    <DataLiterate source={source} frontMatter={frontMatter} />
   )
 }
 
@@ -63,21 +19,12 @@ export const getStaticProps = async ({ params }) => {
   const postFilePath = fs.existsSync(mdxPath) ? mdxPath : mdxPath.slice(0, -1)
   const source = fs.readFileSync(postFilePath)
 
-  const { content, data } = matter(source)
-
-  const mdxSource = await serialize(content, {
-    // Optionally pass remark/rehype plugins
-    mdxOptions: {
-      remarkPlugins: [gfm, toc, slug, footnotes],
-      rehypePlugins: [],
-    },
-    scope: data,
-  })
+  const { mdxSource, frontMatter } = await parse(source)
 
   return {
     props: {
       source: mdxSource,
-      frontMatter: data,
+      frontMatter: frontMatter,
     },
   }
 }
